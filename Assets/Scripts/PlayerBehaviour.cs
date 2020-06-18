c2f24f0396f8871c5526953a1b6021d91e39926a
Saossane <bsaossane@gmail.com>
2020-06-18 13:11:51 +0200

    Bullet Zombie DeathCanvas

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+public class PlayerBehaviour : MonoBehaviour
+{
+    public UnityEvent onDeath;
+
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject gameOverCanvas;
+
+    private Rigidbody2D myRigidbody;
+    private Inputs inputs;
+    private Camera mainCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
