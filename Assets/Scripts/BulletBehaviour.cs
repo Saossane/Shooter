dbe70752e796b1761dbe68309c786501b5a66f64
Saossane <bsaossane@gmail.com>
2020-06-18 13:51:22 +0200

    Bullet Script

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+
+    private Rigidbody2D myRigidbody;
+
+    
+    private void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        var playerGameObject = GameObject.FindWithTag("Player");
+        var playerBehaviour = playerGameObject.GetComponent<PlayerBehaviour>();
+        playerBehaviour.onDeath.AddListener(Die);
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        myRigidbody.velocity = new Vector3
+        {
+            x = speed * Time.fixedDeltaTime,
+            y = 0,
+            z = 0
+        };
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
     }
 }
